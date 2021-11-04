9e6dfdaa4bd3fcebc4d7b4d5ce3d68d55442394e
Dermot <contact@dermotobrien.me>
2021-11-04 16:20:54 +0000

    Initialise and launch UDP Server

--- src/Switcher.ts
+++ src/Switcher.ts
@@ -1,3 +1,21 @@
 
 /** Import dependecies */
 import dgram from "dgram";
+
+/** Initialise UDP Socket */
+const socketPort: number = 51510;
+const Server = dgram.createSocket('udp4');
+/** Launch UDP Socket and HTTP Servers, and listen on given port */
+try {
+    Server.on('listening', (): void => {
+      const address = Server.address();
+      console.log(`Server listening ${address.address}:${address.port}`);
+    });
+  
+    Server.bind(socketPort, (): void => {
+      console.log(`UDP Datagram Server is active at http://localhost:${socketPort}`);
+    });
+  
+  } catch (error: any) {
+    console.error(`An error occurred with starting Server ${error.toString()}`);
+  }
