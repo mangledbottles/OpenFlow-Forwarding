8335db0bfa1144a8d2070a8a14dfb6447ded8c62
Dermot <contact@dermotobrien.me>
2021-11-08 15:07:03 +0000

    Initialise Naive UI throughout Vue

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import Varlet from '@varlet/ui'
+import naive from 'naive-ui'
+// import '@varlet/ui/es/style.js'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(naive)
+    .mount('#app')
