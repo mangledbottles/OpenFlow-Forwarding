d388a38b08b17c66a0e35f924400a8ddbfe10c57
Dermot <contact@dermotobrien.me>
2021-11-19 17:52:36 +0000

    Manage incoming messages from Controller, Clients and Routers

--- Server/src/Router.ts
+++ Server/src/Router.ts
@@ -13,10 +13,46 @@ Router.on('error', (err) => {
     Router.close();
 });
 
+// TODO: How is Client meant to find out what Routers are connected to Switcher and their IP and Port??
+
+// Listen for incoming messages from Clients and the Switcher
 Router.on('message', (msg, senderInfo) => {
-    // console.log(msg.toString))
-    const { type, message } = JSON.parse(msg.toString());
-    console.log({ type, message })
+    // Type -1: An error has occured
+    // Type 0: Message from Server - accepted this as new Router on network
+    // Type 4: Message from Server - received information about a Client
+    // Type 5: Message from Client - received message instructions from Client to send message over the network
+
+    const { address, port } = senderInfo;
+    let { type, message: receivedMessage } = JSON.parse(msg.toString());
+
+    switch (type) {
+        case 0:
+            // Type 0: Message from Server - accepted this as new Router on network
+            console.log("Switcher has accepted this Router");
+            routerId = receivedMessage.message.routerId;
+            let { address: routerAddress, port: routerPort } = receivedMessage.message;
+            console.log(`Router active on ${routerAddress}:${routerPort}`);
+            break;
+        case 4:
+            // Type 4: Message from Server - received information about a Client
+            console.log("Switcher has sent information about a Client");
+            break;
+        case 5:
+            // Type 5: Message from Client - received message instructions from Client to send message over the network
+            console.log("Client has sent message instructions");
+
+            // Request information about Clients from Switcher
+            connectToSwitcher();
+            break;
+        default:
+            console.log("Unknown Message Type received");
+            break;
+    }
+
+
+    console.log(JSON.parse(msg.toString()));
+    // const { origin, serverTime, messageCount, data, message } = JSON.parse(msg.toString());
+    // console.log({ origin, serverTime, messageCount, data, message  })
     // console.log({ message: message.toString(), senderInfo });
 });
 
