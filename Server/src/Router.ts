0ededcc0a10663e430ddd81fc6a60e9eb1a9bfff
Dermot <contact@dermotobrien.me>
2021-11-22 16:53:01 +0000

    Router: Formatting and removing unused code

--- Server/src/Router.ts
+++ Server/src/Router.ts
@@ -13,7 +13,6 @@ Router.on('error', (err) => {
     Router.close();
 });
 
-// TODO: How is Client meant to find out what Routers are connected to Switcher and their IP and Port??
 interface Router {
     routerIn?: String; // eg E1
     routerOut?: String; // eg R2
@@ -42,7 +41,9 @@ Router.on('message', (msg, senderInfo) => {
         case 0:
             // Type 0: Message from Server - accepted this as new Router on network
             console.log("Switcher has accepted this Router");
+
             routerId = receivedMessage.message.routerId;
+            currentRouter.routerId = routerId;
             let { address: routerAddress, port: routerPort } = receivedMessage.message;
             console.log(`Router active on ${routerAddress}:${routerPort}`);
             break;
@@ -59,6 +60,7 @@ Router.on('message', (msg, senderInfo) => {
             break;
         case 6:
             // Type 6: Message from Switcher - updated information about this Router received
+            console.log("Switcher sent updated information about this Router")
             currentRouter = { ...currentRouter, ...receivedMessage };
             break;
 
@@ -67,23 +69,21 @@ Router.on('message', (msg, senderInfo) => {
             break;
     }
 
+    console.log({ currentRouter });
 
     console.log(JSON.parse(msg.toString()));
-    // const { origin, serverTime, messageCount, data, message } = JSON.parse(msg.toString());
-    // console.log({ origin, serverTime, messageCount, data, message  })
-    // console.log({ message: message.toString(), senderInfo });
 });
 
 // Connect router to Switcher
 function connectToSwitcher() {
-    if(routerId) return console.log("Router already connected to Switcher");
+    if (routerId) return console.log("Router already connected to Switcher");
     const message = prepareMessage(1, "Router");
     sendMessageToSwitcher(message);
 }
 
 // Get information about Clients from Switcher from Flow Table
 function getFlowTableFromSwitcher() {
-    if(!routerId) return console.log("Router not connected to Switcher and cannot query");
+    if (!routerId) return console.log("Router not connected to Switcher and cannot query");
     const message = prepareMessage(2, { routerId });
     sendMessageToSwitcher(message);
 }
@@ -92,16 +92,16 @@ function getFlowTableFromSwitcher() {
 function sendMessageToSwitcher(message: Buffer) {
     Router.send(message, switcherPort, 'localhost', (err) => {
         if (err) {
-            console.log('Error sending data to Server')
+            console.log('Error sending data to Switcher')
             Router.close();
         } else {
-            console.log('Data sent to Server')
+            console.log('Data sent to Switcher')
         }
     });
 }
 
 // Prepare message to be sent to Switcher
-function prepareMessage (type: number, message: any) {
+function prepareMessage(type: number, message: any) {
     return Buffer.from(JSON.stringify({ type, message }));
 }
 
