0e413744363c5e405a476be9b6cc53eadeee9e44
Dermot <contact@dermotobrien.me>
2021-11-22 16:51:11 +0000

    Create Router interface and initialise

--- Server/src/Router.ts
+++ Server/src/Router.ts
@@ -14,6 +14,18 @@ Router.on('error', (err) => {
 });
 
 // TODO: How is Client meant to find out what Routers are connected to Switcher and their IP and Port??
+interface Router {
+    routerIn?: String; // eg E1
+    routerOut?: String; // eg R2
+    routerId?: String; // eg R1
+    address?: String; // eg 127.0.0.1
+    port?: number; // eg 69902
+    forwardAddress?: string; // eg 127.0.0.1
+    forwardPort?: number; // eg 61984
+}
+
+let currentRouter: Router = {};
+
 
 // Listen for incoming messages from Clients and the Switcher
 Router.on('message', (msg, senderInfo) => {
